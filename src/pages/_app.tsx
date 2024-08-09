6245b30bbe837553e7e9afe4e685348e93cee74f
Roman Gromov <gromov.r.v.92@gmail.com>
2024-08-09 23:19:52 +0500

    fix: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Rick and Morty</title>
+        <link rel="shortcut icon" href="favicon.ico" />
       </Head>
       <React.StrictMode>
         <ErrorBoundary fallback={<div>Oops! Something went wrong.</div>}>
