04d5450e193a5d21283f0226f469298fa0589c75
Roman Gromov <gromov.r.v.92@gmail.com>
2024-08-18 20:39:57 +0500

    fix: countries select

--- src/components/AutocompleteCountry/AutocompleteCountry.tsx
+++ src/components/AutocompleteCountry/AutocompleteCountry.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Ref, useImperativeHandle, forwardRef } from 'react';
+import { useState, useEffect, Ref, useImperativeHandle, forwardRef, useRef, ChangeEvent } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '@core/store';
 import { fetchCountries } from '@core/slices/countries';
@@ -14,6 +14,9 @@ const AutocompleteCountry = forwardRef(({ value, onChange }: AutocompleteCountry
   const { countries, status } = useSelector((state: RootState) => state.countriesReducer);
   const [query, setQuery] = useState(value || '');
   const [filteredCountries, setFilteredCountries] = useState<string[]>([]);
+  const [selected, setSelected] = useState<string | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
   useEffect(() => {
     if (status === 'idle') {
@@ -22,26 +25,57 @@ const AutocompleteCountry = forwardRef(({ value, onChange }: AutocompleteCountry
   }, [status, dispatch]);
 
   useEffect(() => {
-    if (query) {
+    if (query !== selected) {
       const filtered = countries.filter((country: string) => country.toLowerCase().includes(query.toLowerCase()));
       setFilteredCountries(filtered);
     } else {
       setFilteredCountries([]);
+      setDropdownVisible(false);
     }
   }, [query, countries]);
 
   const handleSelect = (country: string) => {
     setQuery(country);
+    setSelected(country);
     setFilteredCountries([]);
+    setDropdownVisible(true);
     onChange?.(country);
   };
 
   useImperativeHandle(ref, () => ({ value: query }));
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+      setDropdownVisible(false);
+    } else {
+      setDropdownVisible(true);
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setDropdownVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setDropdownVisible(true);
+  };
+
   return (
-    <div className="country-container">
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Select a country" />
-      {filteredCountries.length > 0 && (
+    <div className="country-container" ref={containerRef}>
+      <input type="text" value={query} onChange={handleChange} placeholder="Select a country" />
+      {isDropdownVisible && filteredCountries.length > 0 && (
         <ul style={{ border: '1px solid #ccc', marginTop: '4px' }} className="country-items-list">
           {filteredCountries.map((country) => (
             // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
