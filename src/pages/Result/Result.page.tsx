69bbacc311ec39bd66fcaaeb07f5487a5000f762
Roman Gromov <gromov.r.v.92@gmail.com>
2024-08-18 23:46:50 +0500

    feat: add new highlight

--- src/pages/Result/Result.page.tsx
+++ src/pages/Result/Result.page.tsx
@@ -4,48 +4,39 @@ import { useSelector } from 'react-redux';
 import { useContext } from 'react';
 import { ThemeContext } from '@context/ThemeContext';
 
+const LAST_INDEX = -1;
+const PREV_INDEX = -2;
+
 export default function ResultPage() {
   const { theme } = useContext(ThemeContext);
   const uncontrolledData = useSelector((state: RootState) => state.uncontrolledReducer.data);
   const controlledData = useSelector((state: RootState) => state.controlledReducer.data);
 
-  const lastUncontrolledData = uncontrolledData.at(-1);
-  const lastControlledData = controlledData.at(-1);
+  const lastData = [
+    { title: 'Uncontrolled Form Data', last: uncontrolledData.at(LAST_INDEX), prev: uncontrolledData.at(PREV_INDEX) },
+    { title: 'Controlled Form Data', last: controlledData.at(LAST_INDEX), prev: controlledData.at(PREV_INDEX) },
+  ];
 
   return (
     <div className={`result-page ${theme}`}>
-      <div className="result-column">
-        <h2 className="result-title">Uncontrolled Form Data</h2>
-        {uncontrolledData.length === 0 ? (
-          <p className="result-no-data">No Data</p>
-        ) : (
-          <div className="result-form-data">
-            <p>Name: {lastUncontrolledData?.name || 'N/A'}</p>
-            <p>Age: {lastUncontrolledData?.age || 'N/A'}</p>
-            <p>Email: {lastUncontrolledData?.email || 'N/A'}</p>
-            <p>Gender: {lastUncontrolledData?.gender || 'N/A'}</p>
-            <p>Country: {lastUncontrolledData?.country || 'N/A'}</p>
-            <p>Picture: </p>
-            {lastUncontrolledData?.picture && <img src={lastUncontrolledData.picture} alt="Uploaded" />}
-          </div>
-        )}
-      </div>
-
-      <div className="result-column">
-        <h2 className="result-title">Controlled Form Data</h2>
-        {controlledData.length === 0 ? (
-          <p className="result-no-data">No Data</p>
-        ) : (
-          <div className="result-form-data">
-            <p>Name: {lastControlledData?.name || 'N/A'}</p>
-            <p>Age: {lastControlledData?.age || 'N/A'}</p>
-            <p>Email: {lastControlledData?.email || 'N/A'}</p>
-            <p>Gender: {lastControlledData?.gender || 'N/A'}</p>
-            <p>Country: {lastControlledData?.country || 'N/A'}</p>
-            {lastControlledData?.picture && <img src={lastControlledData.picture} alt="Uploaded" />}
-          </div>
-        )}
-      </div>
+      {lastData.map(({ title, last, prev }) => (
+        <div key={title} className="result-column">
+          <h2 className="result-title">{title}</h2>
+          {!last ? (
+            <p className="result-no-data">No Data</p>
+          ) : (
+            <div className="result-form-data">
+              <p className={prev && prev.name !== last.name ? 'new' : ''}>Name: {last.name || 'N/A'}</p>
+              <p className={prev && prev.age !== last.age ? 'new' : ''}>Age: {last.age || 'N/A'}</p>
+              <p className={prev && prev.email !== last.email ? 'new' : ''}>Email: {last.email || 'N/A'}</p>
+              <p className={prev && prev.gender !== last.gender ? 'new' : ''}>Gender: {last.gender || 'N/A'}</p>
+              <p className={prev && prev.country !== last.country ? 'new' : ''}>Country: {last.country || 'N/A'}</p>
+              <p className={prev && prev.picture !== last.picture ? 'new' : ''}>Picture: </p>
+              <img src={last.picture} alt="Uploaded" />
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
